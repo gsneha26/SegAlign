a633592d01e1d27f0431bd2ff299801ddd8576f7
gsneha26 <gsneha@stanford.edu>
2020-01-28 20:47:43 +0000

    Added header to each segment file

--- segment_printer.cpp
+++ segment_printer.cpp
@@ -3,7 +3,6 @@
 #include "graph.h"
 
 std::mutex io_lock;
-int print_header = 1;
 
 void segment_printer_body::operator()(printer_input input, printer_node::output_ports_type & op){
     auto &payload = get<0>(input); 
@@ -14,29 +13,30 @@ void segment_printer_body::operator()(printer_input input, printer_node::output_
     auto &rc_segments = get<2>(payload);
 
     std::string filename = "tmp"+std::to_string(index)+".segments";
-    FILE* segment_file = fopen(filename.c_str(), "w");
+    FILE* segmentFile = fopen(filename.c_str(), "w");
+    int print_header = 1;
 
     for (auto e: fw_segments) {
 
         if (print_header == 1) {
             io_lock.lock();
-            fprintf(segment_file, "#name1\tstart1\tend1\tname2\tstart2\tend2\tstrand2\tscore\n");
-            printf("%s\n", filename.c_str());
+            fprintf(segmentFile, "#name1\tstart1\tend1\tname2\tstart2\tend2\tstrand2\tscore\n");
             print_header = 0;
             io_lock.unlock();
         }
 
         io_lock.lock();
-        fprintf(segment_file, "ce11.chr1\t%d\t%d\tcb4.chr1\t%d\t%d\t+\t%d\n", (e.ref_start+1), (e.ref_start+e.len), (e.query_start+1), (e.query_start+e.len), e.score);
+        fprintf(segmentFile, "ce11.chr1\t%d\t%d\tcb4.chr1\t%d\t%d\t+\t%d\n", (e.ref_start+1), (e.ref_start+e.len), (e.query_start+1), (e.query_start+e.len), e.score);
         io_lock.unlock();
     }
 
     for (auto e: rc_segments) {
 
         io_lock.lock();
-        fprintf(segment_file, "ce11.chr1\t%d\t%d\tcb4.chr1\t%d\t%d\t-\t%d\n", (e.ref_start+1), (e.ref_start+e.len), (e.query_start+1), (e.query_start+e.len), e.score);
+        fprintf(segmentFile, "ce11.chr1\t%d\t%d\tcb4.chr1\t%d\t%d\t-\t%d\n", (e.ref_start+1), (e.ref_start+e.len), (e.query_start+1), (e.query_start+e.len), e.score);
         io_lock.unlock();
     }
 
+    fclose(segmentFile);
     get<0>(op).try_put(token);
 };
