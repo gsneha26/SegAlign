f187636e618e4737a3519daaccdf09e0105a3159
Ubuntu <ubuntu@ip-172-31-4-145.us-east-2.compute.internal>
2020-03-02 23:51:18 +0000

    install updates

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash -i 
 
-cd $HOME
+CURR=$PWD
+echo $CURR
+
+cd $CURR
+mkdir bin
+
 sudo apt update && sudo apt dist-upgrade
 sudo apt-get install \
     build-essential \
@@ -12,11 +17,10 @@ sudo apt-get install \
     libboost-thread-dev \
     parallel
 
-mkdir bin 
 wget https://github.com/Kitware/CMake/releases/download/v3.16.3/cmake-3.16.3-Linux-x86_64.sh
 chmod +x cmake-3.16.3-Linux-x86_64.sh
 sudo sh cmake-3.16.3-Linux-x86_64.sh
-echo "export PATH="$HOME/cmake-3.16.3-Linux-x86_64/bin/:\$PATH"" >> ~/.bashrc
+echo "export PATH="$CURR/cmake-3.16.3-Linux-x86_64/bin/:\$PATH"" >> ~/.bashrc
 rm cmake-3.16.3-Linux-x86_64.sh
 
 source ~/.bashrc
@@ -29,7 +33,7 @@ cd build
 cmake -DBOND_ENABLE_GRPC=FALSE ..
 make -j
 sudo make install
-cd $HOME
+cd $CURR
 sudo rm -rf bond
 
 wget http://developer.download.nvidia.com/compute/cuda/10.2/Prod/local_installers/cuda_10.2.89_440.33.01_linux.run
@@ -40,13 +44,13 @@ rm cuda_10.2.89_440.33.01_linux.run
 wget http://www.bx.psu.edu/~rsharris/lastz/lastz-1.04.03.tar.gz
 gunzip lastz-1.04.03.tar.gz
 tar -xvf lastz-1.04.03.tar 
-cd $HOME/lastz-distrib-1.04.03/src
+cd $CURR/lastz-distrib-1.04.03/src
 make -j
-cd $HOME
-cp $HOME/lastz-distrib-1.04.03/src/lastz $HOME/bin/
+cd $CURR
+cp $CURR/lastz-distrib-1.04.03/src/lastz $CURR/bin/
 rm -rf lastz-distrib-1.04.03 lastz-1.04.03.tar
 
-cd $HOME/bin/
+cd $CURR/bin/
 wget http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64.v385/faToTwoBit
 chmod +x faToTwoBit
 wget http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64.v385/faSplit
@@ -58,11 +62,11 @@ echo "export PATH="\$HOME/bin/:\$PATH"" >> ~/.bashrc
 
 source ~/.bashrc
 
-cd $HOME/WGA_GPU
+cd $CURR
 git clone https://github.com/01org/tbb
 mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DTBB_ROOT=${PWD}/../tbb ..
 make -j
-cp $HOME/WGA_GPU/build/wga $HOME/bin/
-cp $HOME/WGA_GPU/run_lastz_gpu.sh $HOME/bin/
+cp $CURR/build/wga $CURR/bin/
+cp $CURR/WGA_GPU/run_lastz_gpu.sh $CURR/bin/
