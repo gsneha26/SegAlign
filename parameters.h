e043b9f0911e14a46203b86d66e6137d61d12d33
Ubuntu <ubuntu@ip-172-31-0-170.us-east-2.compute.internal>
2020-03-02 22:47:46 +0000

    paramters file

---
+++ parameters.h
@@ -0,0 +1,21 @@
+#define TRANSITION_MASK 2
+#define NUC 7 
+#define NUC2 49
+#define A_NT 0
+#define C_NT 1
+#define G_NT 2
+#define T_NT 3
+#define L_NT 4
+#define N_NT 5
+#define X_NT 6
+
+#define LASTZ_INTERVAL 12000000
+#define WGA_CHUNK 250000
+#define MAX_SEEDS 13*WGA_CHUNK
+#define MAX_HITS 100000000 
+
+#define MAX_BLOCKS 1<<10
+#define MAX_THREADS 1024 
+#define BLOCK_SIZE 128 
+#define NUM_WARPS 4
+#define TILE_SIZE 32
