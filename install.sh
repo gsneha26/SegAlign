11687341a5dfe580c64c806b0fe99d0ab83f36e6
gsneha26 <gsneha@stanford.edu>
2020-03-03 19:36:41 +0000

    Install.sh final changes

--- install.sh
+++ install.sh
@@ -5,12 +5,9 @@ CURR=$PWD
 sudo apt update 
 sudo apt-get install \
     build-essential \
-    zlib1g \
     clang \
     zlib1g-dev \
-    libboost-dev \
     libboost-all-dev \
-    libboost-thread-dev \
     cmake \
     parallel
 
@@ -22,9 +19,9 @@ cd build
 cmake -DBOND_ENABLE_GRPC=FALSE ..
 make -j
 sudo make install
-cd $CURR
-sudo rm -rf bond
+sudo rm -rf $CURR/bond
 
+cd $CURR
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
 sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
 wget http://developer.download.nvidia.com/compute/cuda/10.2/Prod/local_installers/cuda-repo-ubuntu1804-10-2-local-10.2.89-440.33.01_1.0-1_amd64.deb
@@ -34,26 +31,21 @@ sudo apt-get update
 sudo apt-get -y install cuda
 rm cuda-repo-ubuntu1804-10-2-local-10.2.89-440.33.01_1.0-1_amd64.deb
 
-#wget http://www.bx.psu.edu/~rsharris/lastz/lastz-1.04.03.tar.gz
-#gunzip lastz-1.04.03.tar.gz
-#tar -xvf lastz-1.04.03.tar 
-#cd $CURR/lastz-distrib-1.04.03/src
-#make -j
-#cd $CURR
-#cp $CURR/lastz-distrib-1.04.03/src/lastz $CURR/bin/
-#rm -rf lastz-distrib-1.04.03 lastz-1.04.03.tar
-#
-#cd $CURR/bin/
-#wget http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64.v385/faToTwoBit
-#chmod +x faToTwoBit
-#wget http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64.v385/faSplit
-#chmod +x faSplit
-#wget http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64.v385/twoBitToFa
-#chmod +x twoBitToFa
-#
-#echo "export PATH="$CURR/bin/:\$PATH"" >> ~/.bashrc
-#
-#source ~/.bashrc
+wget http://www.bx.psu.edu/~rsharris/lastz/lastz-1.04.03.tar.gz
+gunzip lastz-1.04.03.tar.gz
+tar -xvf lastz-1.04.03.tar 
+cd $CURR/lastz-distrib-1.04.03/src
+make -j
+cp $CURR/lastz-distrib-1.04.03/src/lastz $CURR/bin/
+rm -rf $CURR/lastz-distrib-1.04.03 lastz-1.04.03.tar
+
+cd $CURR/bin/
+wget http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64.v385/faToTwoBit
+chmod +x faToTwoBit
+wget http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64.v385/faSplit
+chmod +x faSplit
+wget http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64.v385/twoBitToFa
+chmod +x twoBitToFa
 
 cd $CURR
 git clone https://github.com/01org/tbb
