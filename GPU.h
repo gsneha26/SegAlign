d6d7b1d7825875a2b77d8915d09100b252dec36f
Ubuntu <ubuntu@ip-172-31-4-145.us-east-2.compute.internal>
2020-03-07 20:33:25 +0000

    Handling reference issues

--- GPU.h
+++ GPU.h
@@ -14,6 +14,8 @@ typedef size_t(*InitializeProcessor_ptr)(int* sub_mat);
 typedef void(*SendSeedPosTable_ptr)(uint32_t* index_table, uint32_t index_table_size, uint32_t* pos_table, uint32_t ref_size);
 typedef std::vector<hsp> (*SeedAndFilter_ptr)(std::vector<uint64_t> seed_offset_vector, bool rev, uint32_t buffer, uint32_t seed_size, int xdrop, int hspthresh);
 typedef void(*ShutdownProcessor_ptr)();
+typedef void(*clearQuery_ptr)(uint32_t buffer);
+typedef void(*clearRef_ptr)();
 typedef void(*SendRefWriteRequest_ptr)(size_t addr, size_t len);
 typedef void(*SendQueryWriteRequest_ptr)(size_t addr, size_t len, uint32_t buffer);
 
@@ -24,4 +26,6 @@ extern SendSeedPosTable_ptr g_SendSeedPosTable;
 extern SeedAndFilter_ptr g_SeedAndFilter;
 extern SendRefWriteRequest_ptr g_SendRefWriteRequest;
 extern SendQueryWriteRequest_ptr g_SendQueryWriteRequest;
-extern ShutdownProcessor_ptr g_ShutdownProcessor;      
+extern ShutdownProcessor_ptr g_ShutdownProcessor;
+extern clearQuery_ptr g_clearQuery;
+extern clearRef_ptr g_clearRef;
