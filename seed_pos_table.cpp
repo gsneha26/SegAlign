5195c9b70d4c516a3776600229949ccdc985e725
Ubuntu <ubuntu@ip-172-31-4-145.us-east-2.compute.internal>
2020-03-24 20:14:19 +0000

    Changes for variable GPU memory

--- seed_pos_table.cpp
+++ seed_pos_table.cpp
@@ -17,6 +17,7 @@ int SeedPosTable::GetShapeSize() {
 
 SeedPosTable::SeedPosTable(char* ref_str, size_t start_addr, uint32_t ref_length, std::string shape, uint32_t step) {
 
+    int max_pos = 0;
     shape_size_ = shape.length(); 
     int kmer_size = 0;
     for (int i = 0; i < shape_size_; i++) {
@@ -54,6 +55,8 @@ SeedPosTable::SeedPosTable(char* ref_str, size_t start_addr, uint32_t ref_length
     tmp_pos_table_ = (uint32_t*) calloc(num_index, sizeof(uint32_t));
     uint32_t curr_index = 0;
     uint32_t seed, pos; 
+    uint32_t prev_i = 0;
+    uint32_t num_i = 0;
 
     for (uint32_t i = 0; i < num_index; i++) {
         pos  = ((pos_table_[i] << 32) >> 32);
@@ -63,6 +66,11 @@ SeedPosTable::SeedPosTable(char* ref_str, size_t start_addr, uint32_t ref_length
         if (seed > curr_index) {
             for (uint32_t s = curr_index; s < seed; s++) {
                 index_table_[s] = i;
+                num_i = i - prev_i;
+                if(num_i > max_pos)
+                    max_pos = num_i;
+                
+                prev_i = i;
             }
             curr_index = seed;
         }
@@ -72,7 +80,7 @@ SeedPosTable::SeedPosTable(char* ref_str, size_t start_addr, uint32_t ref_length
         index_table_[i] = num_index;
     }
 
-    g_SendSeedPosTable(index_table_, index_table_size_, tmp_pos_table_, num_index);
+    g_SendSeedPosTable(index_table_, index_table_size_, tmp_pos_table_, num_index, max_pos);
 
     free(index_table_);
     free(pos_table_);
