7aa78534b5a874ab1c2fd8d3035404c1b77e4df0
gsneha26 <gsneha@stanford.edu>
2020-05-05 19:49:12 +0000

    finished first version of block reading

--- store.h
+++ store.h
@@ -1,5 +1,15 @@
 #include "DRAM.h"
+#include <vector>
+#include <string>
 
 extern DRAM *ref_DRAM;
 extern DRAM *query_DRAM;
 extern DRAM *query_rc_DRAM;
+extern std::vector<std::string> q_chr_name;
+extern std::vector<size_t>      q_chr_start;
+extern std::vector<size_t>      q_chr_len;
+extern std::vector<size_t>      q_chr_len_padded;
+extern std::vector<std::string> r_chr_name;
+extern std::vector<size_t>      r_chr_start;
+extern std::vector<size_t>      r_chr_len;
+extern std::vector<size_t>      r_chr_len_padded;
