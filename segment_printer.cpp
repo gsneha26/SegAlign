165bc4c0c0c77133d3664fd6ad33dbd7aeae7e6f
gsneha26 <gsneha@stanford.edu>
2020-05-06 06:09:22 +0000

    No files created with 0 HSPs

--- segment_printer.cpp
+++ segment_printer.cpp
@@ -26,36 +26,40 @@ void segment_printer_body::operator()(printer_input input, printer_node::output_
     err_filename = base_filename+".err";
     output_filename  = base_filename+"."+cfg.output_format;
 
-    FILE* segmentFile = fopen(segment_filename.c_str(), "w");
+    uint32_t num_hsps = fw_segments.size() + rc_segments.size();;
 
-    if(cfg.gapped){
-        fprintf(segmentFile, "#name1\tstart1\tend1\tname2\tstart2\tend2\tstrand2\tscore\n");
-    }
+    if(num_hsps > 0){
+        FILE* segmentFile = fopen(segment_filename.c_str(), "w");
 
-    for (auto e: fw_segments) {
-        fprintf(segmentFile, "%s\t%d\t%d\t%s\t%d\t%d\t+\t%d\n", ref_chr.c_str(),(e.ref_start+1), (e.ref_start+e.len+1), query_chr.c_str(), (e.query_start+1), (e.query_start+e.len+1), e.score);
-    }
+        if(cfg.gapped){
+            fprintf(segmentFile, "#name1\tstart1\tend1\tname2\tstart2\tend2\tstrand2\tscore\n");
+        }
 
-    for (auto e: rc_segments) {
-        fprintf(segmentFile, "%s\t%d\t%d\t%s\t%d\t%d\t-\t%d\n", ref_chr.c_str(), (e.ref_start+1), (e.ref_start+e.len+1), query_chr.c_str(), (e.query_start+1), (e.query_start+e.len+1), e.score);
-    }
+        for (auto e: fw_segments) {
+            fprintf(segmentFile, "%s\t%d\t%d\t%s\t%d\t%d\t+\t%d\n", ref_chr.c_str(),(e.ref_start+1), (e.ref_start+e.len+1), query_chr.c_str(), (e.query_start+1), (e.query_start+e.len+1), e.score);
+        }
 
-    fclose(segmentFile);
+        for (auto e: rc_segments) {
+            fprintf(segmentFile, "%s\t%d\t%d\t%s\t%d\t%d\t-\t%d\n", ref_chr.c_str(), (e.ref_start+1), (e.ref_start+e.len+1), query_chr.c_str(), (e.query_start+1), (e.query_start+e.len+1), e.score);
+        }
 
-    if(cfg.gapped){
+        fclose(segmentFile);
 
-        std::string cmd;
+        if(cfg.gapped){
 
-        cmd = "lastz "+cfg.data_folder+"ref/chr"+std::to_string(r_index)+".2bit[nameparse=darkspace] "+cfg.data_folder+"query/chr"+std::to_string(q_index)+".2bit[nameparse=darkspace] --format="+ cfg.output_format +" --ydrop="+std::to_string(cfg.ydrop)+" --gappedthresh="+std::to_string(cfg.gappedthresh)+" 2> "+err_filename;
-        if(cfg.notrivial)
-            cmd = cmd+" --notrivial";
-        if(cfg.scoring_file != "")
-            cmd = cmd+" --scoring=" + cfg.scoring_file;
-        cmd = cmd+" --segments="+segment_filename+" --output="+output_filename;
-            
-	io_lock.lock();
-	printf("%s\n", cmd.c_str());
-	io_lock.unlock();
+            std::string cmd;
+
+            cmd = "lastz "+cfg.data_folder+"ref/chr"+std::to_string(r_index)+".2bit[nameparse=darkspace] "+cfg.data_folder+"query/chr"+std::to_string(q_index)+".2bit[nameparse=darkspace] --format="+ cfg.output_format +" --ydrop="+std::to_string(cfg.ydrop)+" --gappedthresh="+std::to_string(cfg.gappedthresh)+" 2> "+err_filename;
+            if(cfg.notrivial)
+                cmd = cmd+" --notrivial";
+            if(cfg.scoring_file != "")
+                cmd = cmd+" --scoring=" + cfg.scoring_file;
+            cmd = cmd+" --segments="+segment_filename+" --output="+output_filename;
+
+            io_lock.lock();
+            printf("%s\n", cmd.c_str());
+            io_lock.unlock();
+        }
     }
 
     get<0>(op).try_put(token);
